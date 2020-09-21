0a1215f3e7f5da41a80cf830a8882d8e80c82c47
Cazuist <sovrus@mail.ru>
2020-09-21 19:02:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+[![Code style: airbnb](https://img.shields.io/badge/code%20style-airbnb-blue.svg?style=flat-square)](https://github.com/airbnb/javascript)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/tbmhtdn0nd4ldtxj?svg=true)](https://ci.appveyor.com/project/Cazuist/ahj-8-dnd-trello)
+
+[GH-Pages](https://cazuist.github.io/ahj-8_dnd_trello)
+
+# Домашнее задание к лекции "DnD"
+## 1. Trello
---
+++ src/js/functions.js
@@ -0,0 +1,16 @@
+export function moveAt(element, deltas) {
+  // eslint-disable-next-line no-restricted-globals
+  element.style.left = `${event.clientX - deltas.x}px`;
+
+  // eslint-disable-next-line no-restricted-globals
+  element.style.top = `${event.clientY - deltas.y}px`;
+}
+
+export function getPosition(event, target) {
+  const targetRect = target.getBoundingClientRect();
+
+  if (event.clientY <= targetRect.top + targetRect.height / 2) {
+    return true;
+  }
+  return false;
+}
